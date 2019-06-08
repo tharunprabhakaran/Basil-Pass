d1553bd91710c695c8e5aed3e50627a5b83f8e3d
Tharun <tharun@Tharuns-MacBook-Pro.local>
2019-06-08 15:45:23 +0530

    Documented

--- src/basilPass.py
+++ src/basilPass.py
@@ -1,13 +1,28 @@
+"""
+Name : Basil-Pass
+Version : 0.1
+Description : Exposing files/folders over local network 
+Python : 2.7.x
+"""
+# Imports
+import socket
 from twisted.web.server import Site
 from twisted.web.static import File
 from twisted.internet import reactor
-import socket
-print "\n\n\t\t\t Basil-Pass v0.1 \n\n" 
-sharingLocation = raw_input("Location or file to share : ")
+
+#Splash Message
+print "\n\n\t\t\t Basil-Pass v0.1 \n\n"  
+
+#Get location of file/folder to be shared
+sharingLocation = raw_input("Location or file to share : ") 
 print "\n"
+
+#Get port number to expose the files 
 port = raw_input("Enter a Port : ")
-resource = File(str(sharingLocation))
-factory = Site(resource)
-reactor.listenTCP(int(port),factory)
-print "Files Exposed on "+socket.gethostbyname(socket.gethostname())+":"+port
-reactor.run()
+
+#Creating a Resource | Twisted Componenet
+resource = File(str(sharingLocation))   #Create an File instance to be served over TCP
+factory = Site(resource)                #Creates an File instance to be served over TCP
+reactor.listenTCP(int(port),factory)    #Creates an Site where all resources are stored and can be exposed over any endpoint through TCP
+print "Exposing resource on "+socket.gethostbyname(socket.gethostname())+":"+port #Server initiating message
+reactor.run() #Initiating server
