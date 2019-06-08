bc565d86491cc7d3380e4ed847a618b63e298bd8
Tharun <tharun@Tharuns-MacBook-Pro.local>
2019-06-08 15:22:14 +0530

    Dynamic location and Port : Update

--- src/basilPass.py
+++ src/basilPass.py
@@ -1,8 +1,13 @@
 from twisted.web.server import Site
 from twisted.web.static import File
 from twisted.internet import reactor
-
-resource = File('/Users/tharun/Downloads/Dont')
+import socket
+print "\n\n\t\t\t Basil-Pass v0.1 \n\n" 
+sharingLocation = raw_input("Location or file to share : ")
+print "\n"
+port = raw_input("Enter a Port : ")
+resource = File(str(sharingLocation))
 factory = Site(resource)
-reactor.listenTCP(8889,factory)
+reactor.listenTCP(int(port),factory)
+print "Files Exposed on "+socket.gethostbyname(socket.gethostname())+":"+port
 reactor.run()
